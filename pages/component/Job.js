e8172e67c0d809299631cc5a3e5a84e69e149117
singhcodex <singh.hessen@gmail.com>
2023-04-24 16:29:52 +0200

    jobs listed and single job page

--- pages/component/Job.js
+++ pages/component/Job.js
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 const Job = ({job}) => {
     return (
         <div className="pl-16 pr-16 mb-4 mt-20">
-            <h2 className="underline text-xl font-bold">{job.title}</h2>
+            <h2 className="underline text-xl font-bold">
+                <Link href={`/job/${job.id}`} className="hover:text-sky-600">{job.title}</Link>
+            </h2>
             <p className="text-base font-normal mt-3">{job.description}</p>
 
             <div className="mt-4">
---
+++ pages/job/[id].js
@@ -0,0 +1,38 @@
+import { getJob } from "@/lib/data"
+import prisma from "@/lib/prisma"
+import Link from "next/link"
+
+export default function Job({job}){
+    return (
+       <>
+       <div className="container m-auto">
+        <div className="mt-6 mb-5">
+        <Link className="border text-center rounded-full px-10 py-2 hover:bg-slate-700 hover:text-white" href={`/`}>Back</Link>    
+        </div>
+       
+
+        <div className="card border px-3 py-4">
+            <h2 className="text-center mb-5">{job.title}</h2>
+            <p>{job.description}</p>
+
+            <div className="mt-4">
+                <p className="inline">Posted By </p>
+                <Link href={`/company/${job.author.id}`}>
+                    <span className="underline hover:text-sky-500">{job.author.name}</span>
+                </Link>
+            </div>
+        </div>
+       </div>
+       </>
+    )
+}
+
+
+export async function getServerSideProps(context){
+    let job = await getJob(context.params.id, prisma)
+    job = JSON.parse(JSON.stringify(job))
+    
+    console.log(context.params.id)
+
+    return {props: {job}}
+}
