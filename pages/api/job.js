83cef09a659e81f865f1dfe89f12f05b77fe3608
singhcodex <singh.hessen@gmail.com>
2023-04-25 12:27:04 +0200

    post job backend complete

---
+++ pages/api/job.js
@@ -0,0 +1,53 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "./auth/[...nextauth]"
+import prisma from "@/lib/prisma"
+
+
+export default async function handler(req, res){
+    if(req.method !== 'POST'){
+        return res.status(501).end()
+    }
+
+    const session = await getServerSession(req, res, authOptions)
+
+    if(!session) return res.status(401).json({message: 'Not logged in'})
+
+    const user = await prisma.user.findUnique({
+        where: { id: session.user.id}
+    })
+
+    if(!user) return res.status(401).json({message: 'User not Found'})
+
+    if(req.method === 'POST'){
+      if(!req.body.title){
+        return res.status(400)
+        .json({message: 'Required parameter title missing'})
+      }
+      if(!req.body.description){
+        return res.status(400)
+        .json({message: 'Required parameter description missing'})
+      }
+      if(!req.body.location){
+        return res.status(400)
+        .json({message: 'Required parameter location missing'})
+      }
+      if(!req.body.salary){
+        return res.status(400)
+        .json({message: 'Required parameter salary missing'})
+      }
+
+      await prisma.job.create({
+        data: {
+            title: req.body.title,
+            description: req.body.description,
+            location: req.body.location,
+            salary: req.body.salary,
+            author: {
+                connect: {id: user.id},
+            },
+        }
+      })
+
+      res.status(200).end()
+    }
+}
