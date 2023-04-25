0b6b7503b1f760a72bf345e20a6bada9ee08e0dc
singhcodex <singh.hessen@gmail.com>
2023-04-25 10:07:29 +0200

    session define on setup page and re-route

--- pages/setup.js
+++ pages/setup.js
@@ -1,18 +1,50 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 
 export default function Setup(){
+  
+    const router = useRouter()
+    const {data: session, status} = useSession()
+    const loading = status === 'loading'
+
     const [name, setName] = useState('')
     const [company, setCompany] = useState(false)
 
+    if(loading) return null
+
+    if(!session && !session.user){
+        router.push('/')
+        return null
+    }
+
+    if(!loading && session && session.user.name){
+        router.push('/')
+    }
+
     return (
-        <form className="mt-10 ml-20">
-            <div className="flex-1 mb-5">
-                <div className="form-group">
+        <>
+        <div className="container m-auto">
+        <form className="mt-10 ml-20" 
+        onSubmit={ async (e) => {
+            e.preventDefault()
+            await fetch('/api/setup', {
+                body: JSON.stringify({name, company}),
+                headers: {'Content-Type': 'application/json'},
+                method: 'POST',
+            })
+            session.user.name = name
+            session.user.company = company
+            router.push('/')
+        }}>
+            <div className="card border px-5 py-4">
+                <legend className="font-bold text-xl text-center mb-5">Setup User Name or Company</legend>
+                <div className="form-group text-center">
                     <label className="mx-4">Add your Name</label>
                     <input className="form-input px-4 py-3 rounded border" type="text" name="name" onChange={(e) => setName(e.target.value)}/>   
                 </div>
-                <div className="my-5">
+                <div className="my-5 text-center">
                     <label className="mx-4">Check this box if you're a Company and you want to post a Jobs </label>
                     <input 
                     type="checkbox"
@@ -21,7 +53,15 @@ export default function Setup(){
                     onChange={(e) => setCompany(!company)}
                     className="px-4 py-3 rounded border"/>
                 </div>
+                <div className="text-center">
+                    <button className="border rounded-full px-8 py-2 bg-green-500 hover:bg-green-600">Save</button>
+                </div>
+               
             </div>
         </form>
+        </div>
+        
+        </>
+        
     )
 }
